3e1e902e576efc294209b6e8d2ec90160a62c82a
Danil <workdanilput@mail.ru>
2024-03-25 12:43:52 +0300

    redesian code

--- math/IDZ4/src/main/java/org/example/Main.java
+++ math/IDZ4/src/main/java/org/example/Main.java
@@ -1,12 +1,21 @@
 package org.example;
-import java.lang.Math;;
+import java.lang.Math;
 import java.util.Random;
 import java.util.Scanner;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.print("Введите количество равномерных промежутков: ");
-        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         Integer n = scanner.nextInt();
         Double[][] array = new Double[n][2];
@@ -34,22 +43,88 @@ public class Main {
             array[i] = part;
         }
         //Метод прямоугольников
+        //Выбираем левую точку отрезка
+        System.out.println("Площадь при выборе левых точек: " + rectangle_method_left(n, array, delta));
+
+        System.out.println("Площадь при выборе правых точек: " + rectangle_method_right(n, array, delta));
+        //Выбираем рандомную точку из отрезка
+        System.out.println("Площадь при выборе рандомных точек из отрезка: " + rectangle_method_random(n, array, delta));
+
+
+        //Метод трапеций
+        System.out.println("Площадь при рассчете с помошью метода трапеции: " + trapezoid_method(n, array, delta));
+
+        //Метод Симпсона
+        System.out.println("Площадь при рассчете с помошью метода Симсона: " + simson_method(n, array, delta, end));
+
+        XYSeries maeSeries = new XYSeries("MAE");
+        XYSeries mseSeries = new XYSeries("MSE");
+        for (int i=0; i < n; i++) {
+
+        }
+        // Добавляем данные для MAE
+        maeSeries.add(1, 0.5);
+        maeSeries.add(2, 0.3);
+        maeSeries.add(3, 0.2);
+        maeSeries.add(4, 0.1);
+
+        // Добавляем данные для MSE
+        mseSeries.add(1, 0.3);
+        mseSeries.add(2, 0.2);
+        mseSeries.add(3, 0.15);
+        mseSeries.add(4, 0.1);
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(maeSeries);
+        dataset.addSeries(mseSeries);
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "MAE vs MSE", "Epochs", "Error", dataset, PlotOrientation.VERTICAL, true, true, false);
+
+        XYPlot plot = chart.getXYPlot();
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesLinesVisible(0, true);
+        renderer.setSeriesShapesVisible(0, true);
+        renderer.setSeriesLinesVisible(1, true);
+        renderer.setSeriesShapesVisible(1, true);
+        plot.setRenderer(renderer);
+
+        JFrame frame = new JFrame("MAE vs MSE Chart");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        frame.setContentPane(chartPanel);
+        frame.setVisible(true);
+    }
+
+    private static double function(double x){
+        return 1 / Math.sqrt(x);
+    }
+
+    private static double rectangle_method_left(int n, Double[][] array, double delta){
         double sum = 0;
         //Выбираем левую точку отрезка
         for (int i = 0; i < n; i++) {
             double f = function(array[i][0]);
             sum = sum + f*delta;
         }
-        System.out.println("Площадь при выборе левых точек: " + sum);
-        sum = 0;
+        return sum;
+    }
+
+    private static double rectangle_method_right(int n, Double[][] array, double delta){
+        double sum = 0;
         //Выбираем правую точку отрезка
         for (int i = 0; i < n; i++) {
             double f = function(array[i][1]);
             sum = sum + f*delta;
         }
-        System.out.println("Площадь при выборе правых точек: " + sum);
-        sum = 0;
-        //Выбираем рандомную точку из отрезка
+        return sum;
+    }
+
+    private static double rectangle_method_random(int n, Double[][] array, double delta){
+        double sum = 0;
+        Random random = new Random();
         for (int i = 0; i < n; i++) {
             double minRange = array[i][0];
             double maxRange = array[i][1];
@@ -57,20 +132,19 @@ public class Main {
             double f = function(randomNumInRange);
             sum = sum + f*delta;
         }
-        System.out.println("Площадь при выборе рандомных точек из отрезка: " + sum);
-
-
-        //Метод трапеций
-        sum = 0;
+        return sum;
+    }
+    private static double trapezoid_method(int n, Double[][] array, double delta){
+        double sum = 0;
         for (int i=0; i < n; i++) {
             double f_i_1 = array[i][0];
             double f_i = array[i][1];
             sum = sum + (function(f_i) + function(f_i_1)) * (delta / 2);
         }
-        System.out.println("Площадь при рассчете с помошью метода трапеции: " + sum);
-
-        //Метод Симпсона
-        sum = 0;
+        return sum;
+    }
+    private static double simson_method(int n, Double[][] array, double delta, double end){
+        double sum = 0;
         for (int i=0; i < n; i++) {
             double f_i_1 = array[i][0];
             double f_i = array[i][1];
@@ -83,10 +157,6 @@ public class Main {
             }
             sum = sum + (function(f_i_1) + 4*function((f_i + f_i1) / 2) + function(f_i)) * delta / 6;
         }
-        System.out.println("Площадь при рассчете с помошью метода Симсона: " + sum);
-    }
-
-    private static double function(double x){
-        return 1 / Math.sqrt(x);
+        return sum;
     }
 }
