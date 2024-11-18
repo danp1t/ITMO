a710794be4ea40787d0e163963102a1f24db2111
Данил <workdanilput@mail.ru>
2024-11-18 10:26:49 +0300

    final commit

--- web/lab3/lab3/src/main/java/org/example/ResultService.java
+++ web/lab3/lab3/src/main/java/org/example/ResultService.java
@@ -3,15 +3,21 @@ package org.example;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import jakarta.persistence.PersistenceContext;
 
 @Stateless
 public class ResultService {
 
-    @PersistenceContext(unitName = "myPersistenceUnit")
-    private EntityManager entityManager;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("lab3_123");
+
 
     public void saveResult(ResultBean result) {
-        entityManager.persist(result);
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(result);
+        em.getTransaction().commit();
     }
+
 }
