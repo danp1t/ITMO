a74e33419a3d210ddaf176d8f76efd579c7c3dcc
Данил <workdanilput@mail.ru>
2024-12-21 14:57:06 +0300

    delete much C builder code

--- ЯП/lab4/out/build/debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/danp1t/github/assignment-4-memory-allocator
-# Build directory: /home/danp1t/github/assignment-4-memory-allocator/out/build/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tester")
