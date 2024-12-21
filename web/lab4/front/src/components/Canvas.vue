a40e5c9bd4275c25e0ba8d4a8b7f7dd299ae2994
Данил <workdanilput@mail.ru>
2024-12-21 18:18:16 +0300

    I'm mother's son designer

--- web/lab4/front/src/components/Canvas.vue
+++ web/lab4/front/src/components/Canvas.vue
@@ -7,48 +7,59 @@ const emit = defineEmits(['point-clicked']);
 const canvas = ref(null);
 const ctx = ref(null);
 
+const scale = 45;
+
 function drawAxes(R) {
   ctx.value.clearRect(0, 0, canvas.value.width, canvas.value.height);
+  const halfWidth = canvas.value.width / 2;
+  const halfHeight = canvas.value.height / 2;
+  const width = canvas.value.width;
+  const height = canvas.value.height;
   const halfR = R / 2;
   ctx.value.beginPath();
-  ctx.value.moveTo(200, 0);
-  ctx.value.lineTo(200, 400); // Ось Y
-  ctx.value.moveTo(0, 200);
-  ctx.value.lineTo(400, 200); // Ось X
+  ctx.value.moveTo(halfHeight, 0);
+  ctx.value.lineTo(halfHeight, height); // Ось Y
+  ctx.value.moveTo(0, halfWidth);
+  ctx.value.lineTo(width, halfWidth); // Ось X
   ctx.value.stroke();
   ctx.value.fillStyle = 'black';
-  ctx.value.fillText(-R, 200 - R * 60, 215);
-  ctx.value.fillText(R, 200 + R * 60, 215);
-  ctx.value.fillText(halfR, 200 + halfR * 60, 215);
-  ctx.value.fillText(-halfR, 200 - halfR * 60, 215);
-  ctx.value.fillText(-R, 205, 200 + R * 60);
-  ctx.value.fillText(R, 205, 200 - R * 60);
-  ctx.value.fillText(-halfR, 205, 200 + halfR * 60);
-  ctx.value.fillText(halfR, 205, 200 - halfR * 60);
-  ctx.value.fillText("y", 205, 8);
-  ctx.value.fillText("x", 400, 215);
+  ctx.value.fillText(-R, halfHeight - R * scale, halfHeight + 15);
+  ctx.value.fillText(R, halfHeight + R * scale, halfHeight + 15);
+  ctx.value.fillText(halfR, halfHeight + halfR * scale, halfHeight + 15);
+  ctx.value.fillText(-halfR, halfHeight - halfR * scale, halfHeight + 15);
+  ctx.value.fillText(-R, halfHeight + 5, halfHeight + R * scale);
+  ctx.value.fillText(R, halfHeight + 5, halfHeight - R * scale);
+  ctx.value.fillText(-halfR, halfHeight + 5, halfHeight + halfR * scale);
+  ctx.value.fillText(halfR, halfHeight + 5, halfHeight - halfR * scale);
+  ctx.value.fillText("y", halfHeight + 5, 8);
+  ctx.value.fillText("x", width, halfHeight + 15);
 }
 
 function drawShapes(R) {
-  const scale = 60; // Масштаб для отображения
+const halfWidth = canvas.value.width / 2;
+  const halfHeight = canvas.value.height / 2;
+  const width = canvas.value.width;
+  const height = canvas.value.height;
+  const halfR = R / 2;
+   // Масштаб для отображения
   // Первая четверть: часть окружности
   ctx.value.beginPath();
-  ctx.value.arc(200, 200, R * scale, -(Math.PI * 0.5), 0); // Часть окружности сверху
-  ctx.value.lineTo(200, 200);
+  ctx.value.arc(halfHeight, halfHeight, R * scale / 2, 0, (Math.PI * 0.5)); // Часть окружности сверху
+  ctx.value.lineTo(halfHeight, halfHeight);
   ctx.value.fillStyle = 'rgba(51, 153, 255, 0.5)';
   ctx.value.fill();
   ctx.value.stroke();
   // Вторая четверть: прямоугольник
   ctx.value.beginPath();
-  ctx.value.rect(200 - (R * scale) / 2, 200 - R * scale, (R * scale) / 2, R * scale);
+  ctx.value.rect(halfHeight + (R * scale), halfHeight - R * scale, -(R * scale), R * scale);
   ctx.value.fillStyle = 'rgba(51, 153, 255, 0.5)';
   ctx.value.fill();
   ctx.value.stroke();
   // Третья четверть: треугольник
   ctx.value.beginPath();
-  ctx.value.moveTo(200 - R * scale, 200);
-  ctx.value.lineTo(200, 200 + R * scale);
-  ctx.value.lineTo(200, 200);
+  ctx.value.moveTo(halfHeight - R * scale, halfHeight);
+  ctx.value.lineTo(halfHeight, halfHeight - R * scale);
+  ctx.value.lineTo(halfHeight, halfHeight);
   ctx.value.closePath();
   ctx.value.fillStyle = 'rgba(51, 153, 255, 0.5)';
   ctx.value.fill();
