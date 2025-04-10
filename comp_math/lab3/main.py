87df1f015b0a9ebb51c5b8e4b0c3664d7c4f9e5d
Данил <workdanilput@mail.ru>
2025-04-10 07:40:48 +0300

    fix lab3 comp_math

--- comp_math/lab3/main.py
+++ comp_math/lab3/main.py
@@ -1,10 +1,56 @@
 import other_command
 import command
+import readline
 
+
+class SimpleCompleter:
+    def __init__(self, options):
+        self.options = sorted(options)
+        self.matches = []
+
+    def complete(self, text, state):
+        if state == 0:
+            if text:
+                self.matches = [s for s in self.options if s.startswith(text)]
+            else:
+                self.matches = self.options[:]
+        return self.matches[state] if state < len(self.matches) else None
+
+
+def input_loop():
+    # Инициализация автодополнения
+    completer = SimpleCompleter([
+        '/help', '/exit', '/info', '/start',
+        '/clear', '/choice_equations',
+        '/input_interval', '/input_epsilon'
+    ])
+
+    readline.set_completer(completer.complete)
+    readline.parse_and_bind('tab: complete')
+    readline.set_completer_delims(' \t\n')  # Разделители для автодополнения
+
+    while True:
+        try:
+            line = input('!("/exit" to quit) Введите команду: => ')
+            print(f'Отправка: {line}')
+
+            if line.strip() == '/exit':
+                break
+
+            # Обработка команды
+            other_command.validate_task(line)
+
+        except EOFError:
+            break
+        except KeyboardInterrupt:
+            print("\nВыход...")
+            break
+
+
+# Инициализация
 other_command.info_about_lab()
 other_command.welcome_message()
 command.help()
 
-while True:
-    task = input("Введите команду: ")
-    other_command.validate_task(task)
+# Запуск основного цикла
+input_loop()
