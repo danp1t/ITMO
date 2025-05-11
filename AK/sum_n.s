71995c4dabaefbf4e04f699efcf33d9ce9910345
Данил <workdanilput@mail.ru>
2025-05-11 15:25:26 +0300

    AK code done

--- AK/sum_n.s
+++ AK/sum_n.s
@@ -9,20 +9,33 @@ _start:
     lui      t0, %hi(input_addr)             
     addi     t0, t0, %lo(input_addr)         
     lw       t0, 0(t0)                   
-    lw       t1, 0(t0)                       
+    lw       t1, 0(t0)          
 
-sum_begin:
-    addi    t2, zero, 0                  
+    addi    t3, zero, 0                   
+    beqz     t1, negative_input 
+    bgt t3, t1, negative_input 
 
-sum_loop:
-    beqz    t1, sum_end                  
-    add     t2, t2, t1    
-    addi    t1, t1, -1
-    j       sum_loop
+    addi    t2, t1, 1                   
+    mul     t3, t1, t2
+    mulh   t5, t1, t2
+    bgt t5, zero, overflow_error
+    addi    t4, t4, 1    
+    sra    t3, t3, t4     
 
-sum_end:
-    lui     t0, %hi(output_addr)
+save_result:
+    lui     t0, %hi(output_addr)         
     addi    t0, t0, %lo(output_addr)
-    lw      t0, 0(t0) 
-    sw      t2, 0(t0)
-    halt            
+    lw      t0, 0(t0)
+    sw      t3, 0(t0)                   
+    halt
+
+negative_input:
+    addi    t3, t3, -1    
+    j save_result
+
+overflow_error:
+    lui     t3, 0xCCCCC
+    addi    t3, t3, 0xCCC 
+    j save_result      
+
+        
