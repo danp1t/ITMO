894fcb724b69e003c9dd40b2850679be0b946539
Данил Путинцев <workdanilput@mail.ru>
2025-09-24 09:18:47 +0300

    Сделал отправку данных с фронтенда

--- information_system/labs/lab1/lab1/front/src/main.js
+++ information_system/labs/lab1/lab1/front/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from 'axios';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+app.config.globalProperties.$http = axios;
+
+app.use(router).mount('#app');
