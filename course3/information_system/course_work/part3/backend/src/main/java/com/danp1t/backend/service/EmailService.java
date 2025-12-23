dace59f47c4648396c9d33fe1f031bc6811ccb56
Danil Putintsev <workdanilput@mail.ru>
2025-12-23 18:06:23 +0300

    Отправка кода на почту в бекенде реализована

--- course3/information_system/course_work/part3/backend/src/main/java/com/danp1t/backend/service/EmailService.java
+++ course3/information_system/course_work/part3/backend/src/main/java/com/danp1t/backend/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.danp1t.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,12 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendVerificationEmail(String to, String verificationCode) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject("Email Verification");
         message.setText("Your verification code is: " + verificationCode);
@@ -21,6 +26,7 @@ public class EmailService {
 
     public void sendPasswordResetEmail(String to, String resetToken) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject("Password Reset");
         message.setText("Your password reset token is: " + resetToken +
